08eae99c1e1a3ccd0c3406f742e667b91081e4d1
Kim Do <kimdo@d-172-25-230-185.dhcp.virginia.edu>
2021-03-10 14:51:15 -0500

    integrate google log in
    Used Google API

--- buddyfinder/settings.py
+++ buddyfinder/settings.py
@@ -34,6 +34,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',   # <--
+    'social_app',   # <--
+    'allauth',   # <--
+    'allauth.account',   # <--
+    'allauth.socialaccount',   # <--
+    'allauth.socialaccount.providers.google',   # <--
 ]
 
 MIDDLEWARE = [
@@ -123,3 +130,23 @@ try:
     django_heroku.settings(locals())
 except ImportError:
     found = False
+
+AUTHENTICATION_BACKENDS = (
+ 'django.contrib.auth.backends.ModelBackend',
+ 'allauth.account.auth_backends.AuthenticationBackend',
+ )
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
