c7dad7b274b9df38f32e8e363e8a80d876462623
matt-koehler <78246957+matt-koehler@users.noreply.github.com>
2021-03-29 21:42:17 -0400

    creating profile ugly

--- social_app/views.py
+++ social_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView,CreateView,FormView
 from .models import *
 from django.views import generic
-
 from social_app.models import Profile
 
 
@@ -15,3 +14,13 @@ class ProfileView(generic.ListView):
         context = super(ProfileView,self).get_context_data(*args,**kwargs)
         context['exist'] = exist
         return context
+
+def profile_view(request):
+    context = {}
+    form = ProfileForm(request.POST)
+
+    if form.is_valid():
+        form.save()
+
+    context['form'] = form
+    return render(request, "social_app/profile.html", context)
