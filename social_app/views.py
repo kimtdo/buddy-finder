7b1c22a9851e184cac8f74a071761a377e204e70
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-01 13:39:20 -0400

    fixed migration stuff

--- social_app/views.py
+++ social_app/views.py
@@ -18,6 +18,7 @@ class ProfileView(ListView):
         return context
     def post(self,request,*args,**kwargs):
         if self.request.method=='POST':
+            n = self.request.POST.get('name')
             a = self.request.POST.get('gender')
             b = self.request.POST.get('bio')
             c = self.request.POST.get('interests')
