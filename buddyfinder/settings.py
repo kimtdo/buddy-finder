62b8fe411400fd5dcd682adec5dc5d3d3b3d6d45
Kim Do <kimdo@Kims-MacBook-Pro.local>
2021-04-02 16:15:08 -0400

    fix requirements.txt

--- buddyfinder/settings.py
+++ buddyfinder/settings.py
@@ -34,8 +34,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
-
     'django.contrib.sites',  # <--
     'social_app',  # <--
     'allauth',  # <--
