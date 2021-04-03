51b29853a990d38eb406fe88bbea1957694bb356
matt-koehler <78246957+matt-koehler@users.noreply.github.com>
2021-04-03 15:57:46 -0400

    add test

--- social_app/tests.py
+++ social_app/tests.py
@@ -1,10 +1,20 @@
 from django.test import TestCase
+from social_app.models import Profile
+from social_app.views import ProfileView
 
 # Create your tests here.
-#class display_viewTests(TestCase):
 
-   # def test_no_inputs(self): #test when you submit empty create profile form
-       # self.assertTrue()
+class ProfileTests(TestCase):
+    def setUp(self):
+        Profile.objects.create(name = "test", bio = "bio", gender = "gender")
 
-    #def test_all_inputs(self): #test when you submit full profile form
-       # self.assertTrue()
+    def test_profile_fields(self): #test when you submit full profile form
+        t = Profile.objects.get(name = "test")
+        p = Profile
+        p.name = "test"
+        p.bio = "bio"
+        p.gender = "gender"
+        self.assertTrue(p.name == t.name)
+        self.assertTrue(p.bio == t.bio)
+        self.assertTrue(p.gender == t.gender)
+
