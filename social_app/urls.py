1a740a8432df18cb4267c533a6f50d0013207b75
jackliu612 <jackliu612@gmail.com>
2021-04-19 11:21:29 -0400

    added message form without recipient required

--- social_app/urls.py
+++ social_app/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     path('reportuser/', ReportView.as_view(), name="report"),
     path('reported/', TemplateView.as_view(template_name="social_app/done_report.html")),
     path('browse/', TemplateView.as_view(template_name="social_app/browsing.html")),
-    #path('message', AllMessageView.as_view(), name='all_messages'),
+    path('inbox', AllMessageView.as_view(), name='inbox'),
     path('message/<int:rec_id>/', MessageView.as_view(), name='view_messages'),
     path('compose', MessageCreate.as_view(), name='compose'),
-    path('send_friend_request/<int:userID>/',send_friend_request,name='send_friend_request'),
+    path('compose/<int:rec_id>/', MessageCreateSpecific.as_view(), name='compose2'),
+    path('send_friend_request/<int:userID>/',send_friend_request, name='send_friend_request'),
     path('accept_friend_request/<int:requestID>/',accept_friend_request,name='accept_friend_request'),
 ]
